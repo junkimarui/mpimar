290aa3be9e8b92b7013ee09ef357f1b4f2582475
Junki Marui <junkimarui@gmail.com>
2014-06-18 01:38:47 +0900

    deleted unused variables

--- inverted_index.py
+++ inverted_index.py
@@ -8,7 +8,6 @@ class InvertedIndexJob(MapReduceJob):
     def __init__ (self,mapper_num,reducer_num,input_file,out_file):
         MapReduceJob.__init__(self,{"name":"inverted_index","temp_dir":"/tmp","mapper":mapper_num,"reducer":reducer_num,"out_file":out_file})
         self.input_files = input_file.split(",")
-        self.tmp = {}
 
     def distribute(self):
         for input_file in self.input_files:
